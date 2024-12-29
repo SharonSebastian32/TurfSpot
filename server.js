3dfa94289ab4e4a1ce12df06b6c0f02c8fd510e0
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2024-12-29 19:57:56 +0530

    cors policy uodated

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ dotenv.config();
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
 // routes
 app.use("/api", rootRouter);
 
